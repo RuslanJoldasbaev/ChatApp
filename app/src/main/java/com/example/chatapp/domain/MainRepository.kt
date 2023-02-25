6a0359729b2574b8de0b03b78f1ddf6e153e4a35
az1zbek99 <aytmuratovazizbek220@gmail.com>
2023-02-25 10:38:50 -1200

    app1

---
+++ app/src/main/java/com/example/chatapp/domain/MainRepository.kt
@@ -0,0 +1,12 @@
+package com.example.chatapp.domain
+
+import com.example.chatapp.data.models.Group
+import com.example.chatapp.data.models.ResultData
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.flow.flow
+
+class MainRepository(private val fb:FirebaseFirestore,private val rd:FirebaseDatabase){
+
+
+}
